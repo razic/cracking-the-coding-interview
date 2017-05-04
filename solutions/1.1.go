24495d967128dafb56249cc9d37cceafb876c24d
Zachary Adam Kaplan <razic@viralkitty.com>
2017-05-04 01:28:17 +0000

    :lipstick:

--- solutions/1.1.go
+++ solutions/1.1.go
@@ -1,4 +1,4 @@
-// *1.1 Is Unique:* Implement an algorithm to determine if a string has all
+// *Is Unique:* Implement an algorithm to determine if a string has all
 // unique characters. What if you cannot use additional data structures?
 
 package main
