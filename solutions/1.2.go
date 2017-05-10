7320a40fac3025fed365909ecaba5af29144dbaf
Zachary Adam Kaplan <razic@viralkitty.com>
2017-05-10 01:35:25 +0000

    refactor and optimize

--- solutions/1.2.go
+++ solutions/1.2.go
@@ -7,35 +7,52 @@ import (
 	"os"
 )
 
+func checkPermutation(strA, strB string) bool {
+	// if the length isn't the same, it can't be a permutation
+	if len(strA) != len(strB) {
+		os.Exit(1)
+	}
+
+	same := true
+	occurrences := map[byte][2]int{}
+
+	// in this loop we'll count the occurences for each byte in both strings
+	// we'll also determine if the two strings are the same set of bytes
+	for i := 0; i < len(strA); i++ {
+		a := strA[i]
+		b := strB[i]
+		occa := occurrences[a][0]
+		occb := occurrences[b][1]
+
+		// the bytes don't match, they can't be the same string
+		if a != b {
+			same = false
+		}
+
+		occa++
+		occb++
+	}
+
+	// same string, so not a permutation
+	if same == true {
+		return false
+	}
+
+	for _, v := range occurrences {
+		if v[0] != v[1] {
+			return false
+		}
+	}
+
+	return true
+}
+
 func main() {
 	if len(os.Args) < 3 {
 		os.Exit(1)
 	}
 
-	a := os.Args[1]
-	b := os.Args[2]
-
-	if a == b {
+	if !checkPermutation(os.Args[1], os.Args[2]) {
 		os.Exit(1)
-	}
-
-	n := len(a)
-
-	if n != len(b) {
-		os.Exit(1)
-	}
-
-	aocc := make(map[byte]int)
-	bocc := make(map[byte]int)
-
-	for i := 0; i < n; i++ {
-		aocc[a[i]]++
-		bocc[b[i]]++
-	}
-
-	for i := 0; i < n; i++ {
-		if aocc[a[i]] != bocc[a[i]] {
-			os.Exit(1)
-		}
 	}
 }
